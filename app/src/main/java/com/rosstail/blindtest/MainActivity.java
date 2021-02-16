7ea1cd506523ad30fbb53f15cd813558dd509995
Pierre <pierre.decrock@gmail.com>
2021-02-16 19:33:52 +0100

    mainActivity xml & mediaPlayer test with a song

--- app/src/main/java/com/rosstail/blindtest/MainActivity.java
+++ app/src/main/java/com/rosstail/blindtest/MainActivity.java
@@ -1,14 +1,33 @@
 package com.rosstail.blindtest;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playClick();
+            }
+        });
+    }
+
+
+
+    public void playClick(){
+        mp = MediaPlayer.create(MainActivity.this, R.raw.mj_thriller);
+        mp.start();
     }
 }
